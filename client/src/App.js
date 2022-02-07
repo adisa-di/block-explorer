9cf15cd8354c08acb46e7ce3cfbcc96b67ee2309
Adisa Narula <adisan19@gmail.com>
2022-02-07 17:35:23 +0700

    loading state

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,22 @@ import './App.css';
 function App() {
 
   const [blockNo, setBlockNo] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-  
   useEffect(() => {
+    setLoading(true);
     getCurrentBlock()
       .then(({ data }) => {
         const { blockNo } = data;
         setBlockNo(blockNo);
+        setLoading(false);
       });
   }, []);
 
   return (
     <div className="App">
       <div>Latest Block: </div>
-      <div> { blockNo } </div>
+      <div> { loading ?  "Loading..." : blockNo } </div>
     </div>
   );
 }
